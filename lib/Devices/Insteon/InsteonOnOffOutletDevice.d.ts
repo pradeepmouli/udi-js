116389fcd5adde1371f19f0c9a1ebbf27b3445f5
pradeepmouli <pmouli@mac.com>
2024-01-12 14:37:08 -0500

    Cleanup

--- lib/Devices/Insteon/InsteonOnOffOutletDevice.d.ts
+++
@@ -1,8 +0,0 @@
-import { ISY, ISYDevice, Family } from '../../ISY';
-import { InsteonRelayDevice } from './InsteonRelayDevice';
-export declare class InsteonOnOffOutletDevice extends InsteonRelayDevice {
-    outlet1: InsteonRelayDevice;
-    outlet2: InsteonRelayDevice;
-    constructor(isy: ISY, deviceNode: any);
-    addChild(childDevice: ISYDevice<Family.Insteon>): void;
-}

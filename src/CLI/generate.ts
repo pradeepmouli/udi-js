bc1f335645939bd1afdae023566768cb67366bd2
pradeepmouli <pmouli@mac.com>
2024-01-12 14:36:28 -0500

    ZWave Support

---
+++ src/CLI/generate.ts
@@ -0,0 +1,2 @@
+import { generateTemplateClassesFromXSD } from 'xsd2ts';
+generateTemplateClassesFromXSD('./dependency.xsd'); 
---
+++ src/Devices/ZWave/ZWaveLockDevice.ts
@@ -0,0 +1,43 @@
+import { ISY } from '../../ISY';
+import { Commands, States } from '../../ISYConstants';
+import { ISYUpdateableBinaryStateDevice } from '../ISYDevice';
+import { ZWaveBaseDevice } from './ZWaveBaseDevice';
+
+
+export class ZwaveLockDevice extends ISYUpdateableBinaryStateDevice(ZWaveBaseDevice) {
+	constructor (isy: ISY, deviceNode: any
+	) {
+		super(isy, deviceNode);
+	}
+	public sendLockCommand(lockState: any, resultHandler: any) {
+
+			this.sendNonSecureLockCommand(lockState);
+
+			this.sendSecureLockCommand(lockState);
+
+	}
+	get isLocked() {
+		return this.state;
+	}
+
+	public async updateIsLocked(isLocked: boolean) {
+		return super.updateState(isLocked);
+	}
+
+	public async sendNonSecureLockCommand(lockState: any) {
+		if (lockState) {
+			return this.isy.sendNodeCommand(this, Commands.Lock.Lock);
+		}
+		else {
+			return this.isy.sendNodeCommand(this, Commands.Lock.Unlock);
+		}
+	}
+	public async sendSecureLockCommand(lockState: any) {
+		if (lockState) {
+			return this.isy.sendNodeCommand(this, Commands.On, States.SecureLock.Secured);
+		}
+		else {
+			return this.isy.sendNodeCommand(this, Commands.On, States.SecureLock.NotSecured);
+		}
+	}
+}

7c8b2ab41854221b2ecedb8ad44fdf584c1574ea
Pradeep Mouli <pmouli@mac.com>
2024-11-05 12:11:12 -0500

    various updates

--- packages/isy-matter-server/src/server.ts
+++ packages/isy-matter-server/src/server.ts
@@ -1,13 +1,16 @@
-import '@project-chip/matter-node.js';
-import { ServerNode } from '@project-chip/matter.js/node';
-import { ISY } from 'isy-nodejs/ISY';
-import { createServerNode } from 'isy-nodejs/Matter/Server/Server';
+import { error, log } from 'console';
+import { stat, unlink } from 'fs';
+import { type ISY } from 'isy-nodejs/ISY';
+import { type createServerNode as createServerNodeFunction } from 'isy-nodejs/Matter/Server/Server';
 import { createServer } from 'net';
+import { exit } from 'process';
+import { Transform, pipeline } from 'stream';
+import winston from 'winston';
+import {Format} from 'logform'
 import './Utils.js';
+import { type ServerNode } from '@project-chip/matter.js/node';
 
-import { networkInterfaces } from 'os';
-import { Transform } from 'stream';
-import winston, { type LogEntry } from 'winston';
+
 const format = winston.format;
 const myFormat = format.combine(
 	format.splat(),
@@ -18,76 +21,120 @@ const myFormat = format.combine(
 	}),
 	format.colorize({ all: true })
 );
-
-function zPad2(str: number) {
+function zPad2(str) {
 	return str.toString().padStart(2, '0');
 }
-
-let isyConfig: { host: string; username: string; password: string; port: number; protocol: 'http' | 'https' };
-let matterConfig: { host: string; port: number; protocol: string };
+let isyConfig: { host: string; port: number; protocol: "http" | "https"; username: string; password: string; enableWebSocket?: boolean; displayNameFormat?: string; elkEnabled?: boolean; };
+let matterConfig: any;
 let isy: ISY;
-let serverNode: ServerNode;
-
+let serverNode;
 const logger = winston.loggers.add('server', {
 	format: winston.format.label({ label: 'server' }),
 	transports: [new winston.transports.Console({ level: 'info', format: myFormat }), new winston.transports.File({ filename: 'matter-server.log', level: 'debug', format: myFormat })],
 	exitOnError: false,
 	levels: winston.config.cli.levels
 });
-
 const addLogHeaderStream = new Transform({
-	transform(chunk: LogEntry, encoding, callback) {
-		callback(null, "{type: 'log', level: " + chunk.level + ', message: ' + chunk.message + '}\n');
-	}
+	transform(chunk, encoding, callback) {
+		console.log('log entry: ' + JSON.stringify(chunk));
+		//this.push(JSON.stringify({type: 'log', ...chunk}) + '\n');
+		callback(null, JSON.stringify({ type: 'log', ...chunk }) + '\n');
+	},
+	objectMode: true
 });
 
-const server = createServer((socket) => {
-	socket.write('Echo server\r\n');
-	let t = new winston.transports.Stream({ stream: addLogHeaderStream.pipe(socket), level: 'info', format: myFormat });
-	logger.add(t);
-}).on('data', async (data) => {
-	let msg = JSON.parse(data.toString()) as
-		| { type: 'isyConfig'; host: string; username: string; password: string; port: number; protocol: 'http' | 'https' }
-		| { type: 'matterConfig'; host: string; port: number; protocol: string }
-		| { type: 'command'; command: 'start' | 'stop' };
-	if (msg.type === 'isyConfig') {
-		isyConfig = msg;
-	} else if (msg.type === 'matterConfig') {
-		matterConfig = msg;
-	} else if (msg.type === 'command') {
-		if (msg.command === 'start') {
-			if(!isyConfig || !matterConfig) {
-				logger.error('Missing configuration');
-				return;
-			}
-			if(isy || serverNode) {
-				logger.error('Already started');
-				return;
-			}
-			logger.info('Starting matter bridge', { isyConfig, matterConfig });
-			isy = new ISY(isyConfig, logger);
-			await isy.initialize();
-			serverNode = await createServerNode(isy);
-		} else if (msg.command === 'stop') {
-			if(!isy || !serverNode) {
-				logger.error('Not started');
-				return;
-			}
-			logger.info('Stopping matter bridge');
-			serverNode.close();
-			serverNode = undefined;
-			isy = undefined;
-
-		}
+class tagFormat extends Format {
+	override transform = (info, opts) => {
+		info.type = 'log';
+		return info;
 	}
+}
+
+const server = createServer((socket) => {
+	//socket.write('Echo server\r\n');
+	//let loggerStream = pipeline(addLogHeaderStream,socket);
+	let t = new winston.transports.Stream({ stream: addLogHeaderStream.pipe(socket), level: 'info', format: format.combine(format.json(), new tagFormat()) });
+	logger.add(t);
+	logger.info('Client connected');
+	socket.on('data', async (data) => {
+		console.log('Received: ' + data.toString());
+		data.toString().split('\n').forEach(async (line) => {
+		let	msg = JSON.parse(line);
+
+		//console.log(data.toString());
+		if (msg.type === 'isyConfig') {
+			isyConfig = msg;
+			console.log(JSON.stringify(isyConfig));
+		} else if (msg.type === 'matterConfig') {
+			matterConfig = msg;
+			console.log(JSON.stringify(matterConfig));
+		} else if (msg.type === 'command') {
+			if (msg.command === 'start') {
+				if (!isyConfig || !matterConfig) {
+					logger.error('Missing configuration');
+					return;
+				}
+				if (isy || serverNode) {
+					logger.error('Already started');
+					return;
+				}
+				logger.info('Starting matter bridge', { isyConfig, matterConfig });
+				var isyClass : typeof ISY = (await import('isy-nodejs')).ISY
+				isy = new isyClass(isyConfig, logger);
+				await isy.initialize();
+				let createServerNode : typeof createServerNodeFunction = (await import('isy-nodejs/Matter/Server/Server')).createServerNode
+				serverNode = (await createServerNode(isy)) as ServerNode;
+			} else if (msg.command === 'stop') {
+				if (!isy || !serverNode) {
+					logger.error('Not started');
+					return;
+				}
+				logger.info('Stopping matter bridge');
+				serverNode.close();
+				serverNode = undefined;
+				isy = undefined;
+			}
+		}});
+	});
+}).on('end', () => {
+	logger.info('Client disconnected');
+});
+
+server.on('error', (err) => {
+	error('Server error: ' + err);
+	exit(1);
+});
+
+const matterServiceSockPath = '/tmp/matter-service.sock';
+stat(matterServiceSockPath, function (err, stats) {
+	if (err) {
+		// start server
+		console.log('No leftover socket found.');
+
+		server.listen(matterServiceSockPath, () => {
+			console.log('Server bound');
+		});
+	}
+	// remove file then start server
+	console.log('Removing leftover socket.');
+	unlink(matterServiceSockPath, function (err) {
+		if (err) {
+			// This should never happen.
+			console.error(err);
+			process.exit(0);
+		}
+
+		server.listen(matterServiceSockPath, () => {
+			console.log('Server bound');
+		});
+	});
 });
 
 export default async function main() {
 	//n = createServer();
-
 	/*const isy = new ISY({ host: '192.168.1.50', username: 'admin', password: 'qazWSX12', port: 8080, protocol: 'http' }, logger);
-	await isy.initialize();
-	let s = await createServerNode(isy);*/
+    await isy.initialize();
+    let s = await createServerNode(isy);*/
 }
-
 //main();
+//# sourceMappingURL=server.js.map

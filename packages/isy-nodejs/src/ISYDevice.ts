96ee490204d217065b11a57afa4fd94b44fca289
Pradeep Mouli <pmouli@mac.com>
2024-11-23 12:46:45 -0500

    cleanup

--- packages/isy-nodejs/src/ISYDevice.ts
+++ packages/isy-nodejs/src/ISYDevice.ts
@@ -1,13 +1,9 @@
 import type { Command } from './Definitions/Global/Commands.js';
 import type { Driver } from './Definitions/Global/Drivers.js';
-import type { UnitOfMeasure } from './Definitions/Global/UOM.js';
 import type { Category, Family } from './Definitions/index.js';
 import { ISYDeviceNode } from './Devices/ISYDeviceNode.js';
-import type { ISY } from './ISY.js';
 import type { ISYNode } from './ISYNode.js';
 import type { ISYScene } from './ISYScene.js';
-import type { DriverState } from './Model/DriverState.js';
-import type { StringKeys } from './Utils.js';
 
 export interface ISYDevice<T extends Family, D extends ISYNode.DriverSignatures, C extends ISYNode.CommandSignatures, E extends ISYNode.EventSignatures> extends ISYDeviceInfo {
 	// #region Properties (22)

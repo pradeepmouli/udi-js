f967f8055b6b8b44e8f2965807bc82623345671c
Pradeep Mouli <pmouli@mac.com>
2024-12-05 19:24:23 -0500

    cleanup

--- packages/isy-nodejs/dist/esm/types/Matter/Behaviors/index.d.ts
+++ packages/isy-nodejs/dist/esm/types/Matter/Behaviors/index.d.ts
@@ -1,5 +1,6 @@
-export * from './BehaviorRegistry.js';
-export * from './Insteon';
+import './BehaviorRegistry.js';
+export { BehaviorRegistry as Registry } from './BehaviorRegistry.js';
 export * from './ISYBridgedDeviceBehavior.js';
 export * from './ISYClusterBehavior.js';
+export * from './Insteon/index.js';
 //# sourceMappingURL=index.d.ts.map
